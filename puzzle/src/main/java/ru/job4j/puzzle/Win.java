98e822218e2abf819107d5101ae37e669607e460
Jagupov Jurii <project@idefclub.ru>
2020-12-09 00:15:39 +0300

    win method realesation
    Сокобан. Тестовое задание. [#384005]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,29 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
-        return rsl;
+        for(int i = 0; i < board.length; i++){
+            if (checkH(board, i) || checkV(board, i)) {
+                return true;
+            }
+        }
+        return  false;
+    }
+
+    public static boolean checkH(int[][] board, int h) {
+        for(int i = 0; i < board.length; i++){
+            if (board[h][i] == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkV(int[][] board, int v) {
+        for(int i = 0; i < board.length; i++){
+            if (board[i][v] == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
